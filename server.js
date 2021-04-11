fa55a65ec0dd342d1a283aa389128a0a51a010ec
musaa-muhsen <musaamuhsen@gmail.com>
2021-04-11 19:09:36 +0100

    commit

--- server.js
+++ server.js
@@ -8,16 +8,17 @@ app.get("/api/", (request, response) => {
    response.sendFile(__dirname + "/json/data.json");
  });
 
- app.use(express.static("public"))
+
 
 // //console.log(process.env.NODE_ENV);
-// if (process.env.NODE_ENV === "production"){
-
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("public"));
+    app.use(express.static(path.join(__dirname, 'public')));
 //    //app.use(express.static("client/build"));
 //    app.get("*", (req, res) => {
 //      res.sendFile(path.resolve(__dirname, "index.html"));
 //    });
-//  }
+}
 
 
 const port = process.env.PORT || 3333;
